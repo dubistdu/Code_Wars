df31ac117c5a0b80b92c0f94ee601f918895523e
Hyun Frantz <hyunfrantz@gmail.com>
2017-05-22 23:58:30 -0400

    sequence_classifier

---
+++ sequence_classifier.rb
@@ -0,0 +1,21 @@
+def sequence_classifier(arr)
+sorted = arr.sort
+sorted_reverse = sorted.reverse
+sorted_unique = sorted.uniq
+  if sorted == sorted_reverse
+    5
+  elsif sorted_unique == arr
+    1
+  elsif sorted == arr && sorted_unique.count > 1
+    2
+  elsif sorted_reverse .uniq == arr
+    3
+  elsif sorted_reverse  == arr
+    4
+  else
+     0
+  end
+
+end
+
+#sorting too many times will take too long. if sort gets used several times, save it as a variable. DO NOT REPEAT YOURSELF! -> DRY
