22b53de2c6e0e485e9be4172c57116f552f12028
Hyun Frantz <hyunfrantz@gmail.com>
2018-01-01 21:15:29 -0500

    multiple of index

---
+++ multiple_of_index.rb
@@ -0,0 +1,5 @@
+def multiple_of_index (arr)
+  arr.select.with_index {|a,i| i>0 && a%i==0}
+end
+
+# select(merely selects array element that meets the condition) vs map (changes array and return new one)
