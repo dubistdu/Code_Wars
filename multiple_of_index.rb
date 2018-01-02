b71b2d0664009d129ec35d84be0a6527dad9d6bd
Hyun Frantz <hyunfrantz@gmail.com>
2018-01-01 21:19:29 -0500

    spacing in syntax

--- multiple_of_index.rb
+++ multiple_of_index.rb
@@ -1,5 +1,6 @@
 def multiple_of_index (arr)
-  arr.select.with_index {|a,i| i>0 && a%i==0}
+  arr.select.with_index {|a,i| i > 0 && a % i == 0}
 end
 
 # select(merely selects array element that meets the condition) vs map (changes array and return new one)
+# https://www.codewars.com/kata/multiple-of-index/train/ruby
