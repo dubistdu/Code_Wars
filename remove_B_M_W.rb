02857e18577b710cae29661f984fa7b56b402fe4
Hyun Frantz <hyunfrantz@gmail.com>
2018-01-07 12:01:00 -0500

    refactor remove b m w

--- remove_B_M_W.rb
+++ remove_B_M_W.rb
@@ -1,6 +1,7 @@
+# https://www.codewars.com/kata/remove-b-m-w/train/ruby
 def remove_bmw(str)
   unless str.is_a?(String)
     raise ArgumentError.new("This program only works for text.")
   end
-  (str.chars - ["b","m","w", "B","M","W"]).join
+  (str.chars - %w(b m w B M W)).join
 end
