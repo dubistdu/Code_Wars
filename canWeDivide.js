b7bbdcc294e76bcac020181d866498f9932d89ba
Hyun Frantz <hyunfrantz@gmail.com>
2018-01-08 22:55:46 -0500

    can se divide ES 6 syntax

---
+++ canWeDivide.js
@@ -0,0 +1,8 @@
+// ES6
+const isDivideBy = (number, a, b) => number % a == 0 && number % b == 0;
+
+// ES5
+// function isDivideBy(number, a, b) {
+//  var divided  =  (number%a == 0 && number%b == 0)? true : false
+//  return divided
+// }
