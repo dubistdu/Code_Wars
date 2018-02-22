6484acc83f91706b4bc1b20fcfe4e57f80336be2
mama frantz <hyunfrantz@gmail.com>
2018-02-22 10:14:58 -0500

    linter

--- simple_pig_latin.rb
+++ simple_pig_latin.rb
@@ -1,3 +1,3 @@
 def pig_it text
-  text.split.map {|word| word.split(//,2)}.map { |array|array.include?("?") || array.include?("!")? array.first :  array.reverse.join + "ay" }.join(' ')
+  text.split.map {|word| word.split(//,2)}.map { |array|array.include?("?") || array.include?("!") ? array.first : array.reverse.join + "ay" }.join(' ')
 end
