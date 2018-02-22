3216e5c4e8c4d00d5de35fa17072433f79ef811f
mama frantz <hyunfrantz@gmail.com>
2018-02-22 10:03:13 -0500

    simple_pi_update

--- simple_pig_latin.rb
+++ simple_pig_latin.rb
@@ -1,5 +1,3 @@
-# https://www.codewars.com/kata/simple-pig-latin/train/ruby/586d3a4aaa042820d100002a
 def pig_it text
-  text_array = text.split.map {|word| word.split(//,2)}
-  text_array.map { |array|array.include?("?") || array.include?("!") ? array.first : array.reverse.join + "ay" }.join(' ')
+  text.split.map {|word| word.split(//,2)}.map { |array|array.include?("?") || array.include?("!")? array.first :  array.reverse.join + "ay" }.join(' ')
 end
