9476089d5bfef2b666f3c219f6d06b8731d7cbf0
mama frantz <hyunfrantz@gmail.com>
2018-02-23 15:31:14 -0500

    linter fix

--- weight_for_weight.rb
+++ weight_for_weight.rb
@@ -4,7 +4,7 @@
 # order of list is smallest sum first. If there are identical sums, the order is ordered by string.
 # No negative numbers will be given but it is possible that the list is empty
 #
-#Take away
+# Take away
 # Hash doesn't allow identical keys
 # you can sort array and Hash by two values using (&:reverse)
 def order_weight(weights)
@@ -13,5 +13,5 @@ def order_weight(weights)
   # sort an anrray using sub array's both array items
   # make a new array using only the first array item.
   # combine them to a string
-  weights.split(" ").map {|a| [a,a.split("").map {|a| a.to_i}.inject(:+)]}.sort_by(&:reverse).map {|a| a[0]}.join(" ")
+  weights.split(" ").map { |a| [a,a.split("").map { |a| a.to_i }.inject(:+)] }.sort_by(&:reverse).map { |a| a[0] }.join(" ")
 end
