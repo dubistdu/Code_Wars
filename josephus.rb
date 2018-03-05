51c747af5bef69d948ca3b1794d703bc3f962b7d
mama frantz <hyunfrantz@gmail.com>
2018-03-05 15:43:11 -0500

    josephus rubocop + 1st pass of refactoring

--- josephus.rb
+++ josephus.rb
@@ -5,20 +5,30 @@
 
 # return killed
 
+# def josephus(items,k)
+#   killed = []
+#   if items.length == 0
+#     items
+#   else
+#     # loop the following two until items == []
+#     loop do
+#       items = items.rotate(k)
+#       killed << items.pop
+#       if items.length == 0
+#         break
+#       end
+#     end
+#     killed
+#    end
+# end
 
+####  Refactor
 def josephus(items,k)
   killed = []
-  if items.length == 0
-    items
-  else
-    # loop the following two until items == []
-    loop do
-      items = items.rotate(k)
-      killed << items.pop
-      if items.length == 0
-        break
-      end
-    end
-    killed
-   end
+  # loop the following two until items == []
+  until items.length == 0 do
+    items = items.rotate(k)
+    killed << items.pop
+  end
+  killed
 end
