2836e4b05b0fd6eb7f4e0cca278034bf7023573b
Jasmine F <hyunfrantz@gmail.com>
2018-03-06 08:50:20 -0500

    Update digital_root.rb

--- digital_root.rb
+++ digital_root.rb
@@ -1,7 +1,7 @@
 def digital_root(n)
   # n to string then split n then convert to integer
   # sum array
-  # if the (sum's string length) > 1 loop above until sum is 1
+  # if the (sum's string length) > 1 loop above until sum string length is 1
   n = n.to_s.chars.map(&:to_i).inject(:+) while n.to_s.length > 1
   n
 end
