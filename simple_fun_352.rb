e01c88420eaed02e5796c44289e675d58a785956
Jasmine F <hyunfrantz@gmail.com>
2018-03-12 12:58:33 -0400

    Update simple_fun_352.rb

--- simple_fun_352.rb
+++ simple_fun_352.rb
@@ -9,6 +9,7 @@ def isValid(formula)
   return false if !formula.include?(7) && !formula.include?(8)
   # ^^^ why is it not
   # return false if !formula.include?(7) || !formula.include?(8)
+  # because...
   return false if formula.include?(1) && formula.include?(2)
   return false if formula.include?(3) && formula.include?(4)
   return false if (formula.include?(5) && !formula.include?(6)) || (formula.include?(6) && !formula.include?(5))
@@ -23,7 +24,7 @@ end
 ## Original method
 # def isValid(formula)
 #
-#   if !formula.include?(7||8)
+#   if !formula.include?(7&&8)
 #   false
   ## problem with above is that 'include' checks the first condition and returns value
   ## the same with the rest. So include will just check the first number.compare nd return true or false
@@ -41,3 +42,22 @@ end
 #   end
 #
 # end
+
+# Ugly but works
+
+#def isValid(formula)
+
+#   if !formula.include?(7) && !formula.include?(8)
+#      false
+#   elsif formula.include?(1) && formula.include?(2)
+#     false
+#   elsif formula.include?(3) && formula.include?(4)
+#     false
+#   elsif formula.include?(5) && !formula.include?(6)
+#     false
+#   elsif formula.include?(6) && !formula.include?(5)
+#     false
+#   else
+#   true
+#  end
+#end
