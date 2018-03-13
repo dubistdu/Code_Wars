5ae405d118de28cfb7ceaf1e85e93c6a4cff631f
mama frantz <hyunfrantz@gmail.com>
2018-03-13 15:54:16 -0400

    tribonacci refactor

--- tribonacci_sequence.rb
+++ tribonacci_sequence.rb
@@ -1,10 +1,5 @@
 def tribonacci (signature,n)
-  if n == 0
-    []
-  elsif n < 3
-    signature.slice(0,n)
-  else
-    signature.push(signature[ signature.length - 3 ] + signature[ signature.length - 2 ] + signature[ signature.length - 1 ]) until signature.length == n
-    signature
-  end
+  return signature.take(n) if n < 3
+  signature << signature[ signature.length - 3 ] + signature[ signature.length - 2 ] + signature[ signature.length - 1] until signature.length == n
+  return signature
 end
