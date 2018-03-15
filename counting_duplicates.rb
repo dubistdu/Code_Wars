6f1f032491377f1cba318ee42712e25dd436eca7
mama frantz <hyunfrantz@gmail.com>
2018-03-15 08:55:18 -0400

    counting_duplicates

--- counting_duplicates.rb
+++ counting_duplicates.rb
@@ -1,6 +1,12 @@
 # https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
 
+# Refactor
 def duplicate_count(text)
-  texts = text.upcase.split(//)
-  freq = texts.uniq.map { |x| [x, texts.count(x)] }.to_h.values.select{|num| num > 1}.count
+  texts = text.downcase.chars
+  texts.select { |a| texts.count(a) > 1 }.uniq.count
 end
+
+# def duplicate_count(text)
+#   texts = text.upcase.split(//)
+#   freq = texts.uniq.map { |x| [x, texts.count(x)] }.to_h.values.select{|num| num > 1}.count
+# end
