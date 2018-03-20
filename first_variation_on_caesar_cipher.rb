125d895c6f251674bcf6523a913a0f3b47d2301d
mama frantz <hyunfrantz@gmail.com>
2018-03-19 23:05:24 -0400

    minor refactor

--- first_variation_on_caesar_cipher.rb
+++ first_variation_on_caesar_cipher.rb
@@ -5,9 +5,9 @@ def movingShift(s, shift)
   alphaup = ("A".."Z").to_a
 
   new_array = s.chars.each_with_index.map { |a,i|
-    if (/[A-Z]/).match(a) && a.upcase
+    if (/[A-Z]/).match(a)
       alphaup[ (alphaup.index(a).to_i + shift + i) % 26 ]
-    elsif (/[a-z]/).match(a) && a.downcase
+    elsif (/[a-z]/).match(a)
       alphadown[ (alphadown.index(a).to_i + shift + i) % 26 ]
     else
       a
@@ -15,8 +15,12 @@ def movingShift(s, shift)
   # split string by space but do not get rid of empty space
   # group them using array.each_slice(length of array/5).to_a  (it's enumerator)
   # if array items are less than 5, then add ""
-  new_array.count < 5 ? new_array << "" : new_array
-  }.join("").chars.each_slice((s.length.to_f / 5).ceil).to_a.map { |a| a.join("") }
+  }.join
+   .chars
+   .each_slice((s.length.to_f / 5).ceil)
+   .to_a
+   .map(&:join)
+   new_array.count < 5 ? new_array << "" : new_array
 end
 
 def demovingShift(arr, shift)
@@ -25,9 +29,9 @@ def demovingShift(arr, shift)
   alphaup = ("A".."Z").to_a
 
   arr.join("").chars.each_with_index.map { |a,i|
-    if (/[A-Z]/).match(a) && a.upcase
+    if (/[A-Z]/).match(a)
       alphaup[ (alphaup.index(a).to_i - shift - i) % 26 ]
-    elsif (/[a-z]/).match(a) && a.downcase
+    elsif (/[a-z]/).match(a)
       alphadown[ (alphadown.index(a).to_i - shift - i) % 26 ]
     else
       a
