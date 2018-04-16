9c8a433ff772512f909662e61031f9b648bacd41
mama frantz <hyunfrantz@gmail.com>
2018-04-16 17:35:52 -0400

    consonant value js

---
+++ consonantValue.js
@@ -0,0 +1,8 @@
+function solve(s) {
+  let anum= {
+        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11,
+        l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20,
+        u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+    }
+  return Math.max(...s.replace(/[aeiou]/ig, '*').split('*').filter(e => String(e).trim()).map( x => x.split('').map (i => anum[i]).reduce((a,b) => a + b, 0)))
+};
