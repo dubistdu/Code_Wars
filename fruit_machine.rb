1a99dc7d477505368c36c476b695d6be6e8c011c
mama frantz <hyunfrantz@gmail.com>
2018-07-06 14:30:58 -0400

    fruit machine refactor

--- fruit_machine.rb
+++ fruit_machine.rb
@@ -1,17 +1,18 @@
 def fruit(reels, spins)
   point = { "Wild" => 10 ,"Star" => 9,"Bell" => 8,"Shell" => 7,"Seven" => 6,"Cherry" => 5,"Bar" => 4,"King" => 3,"Queen" => 2,"Jack" => 1 }
-  reel1 = reels[0]
-  reel2 = reels[1]
-  reel3 = reels[2]
-  spin_results = [reel1[spins[0]], reel2[spins[1]], reel3[spins[2]]]
-  different_images = spin_results.uniq
-  return 0 if different_images.count == 3
-  if different_images.count == 1
-    point[different_images.first] * 10
-  elsif spin_results.count("Wild") == 1 && different_images.count == 2
-    different_images.delete("Wild")
-    point[ different_images.first ] * 2
+
+  reel1, reel2,reel3 = reels
+
+  images = [reel1[spins[0]], reel2[spins[1]], reel3[spins[2]]]
+  uniq_images = images.uniq
+
+  return 0 if uniq_images.count == 3
+
+  if uniq_images.count == 1
+    point[uniq_images.first] * 10
+  elsif images.count("Wild") == 1 && uniq_images.count == 2
+    point[images.select { |r| images.count(r) == 2 }.first] * 2
   else
-    point[spin_results.select { |r| spin_results.count(r) == 2 }.first]
+    point[images.select { |r| images.count(r) == 2 }.first]
   end
 end
