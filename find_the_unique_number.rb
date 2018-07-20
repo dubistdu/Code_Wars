467cce2f71e635b6048ea58c97fc1014ec9d499b
mama frantz <hyunfrantz@gmail.com>
2018-07-20 00:04:24 -0400

    refactor

--- find_the_unique_number.rb
+++ find_the_unique_number.rb
@@ -1,6 +1,5 @@
 # reference PEDAC note
 
 def find_unique(numbers)
-  array_found = numbers.sort.each_slice(2).to_a.find { |a| a[0] != a[1] }
-  array_found[0]
+  numbers.sort.each_slice(2).find { |a,b| a != b }.first
 end
