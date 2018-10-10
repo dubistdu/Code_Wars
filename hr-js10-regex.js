0f3761d964b57a59a14cce42c3a31bb238bd62d1
mama frantz <hyunfrantz@gmail.com>
2018-10-10 00:16:02 -0400

    Hacker Rank js10 regex

---
+++ hr-js10-regex.js
@@ -0,0 +1,8 @@
+const regexVar = () => {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+  const re = /^([aeiou]).*\1$/
+  return re;
+}
