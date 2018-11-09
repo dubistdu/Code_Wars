04f6cf96573f5f3d789f49f160c23a5b320d6934
mama frantz <hyunfrantz@gmail.com>
2018-11-08 23:09:57 -0500

    double_trouble.rb rubocop

--- double_trouble.rb
+++ double_trouble.rb
@@ -1,15 +1,15 @@
 ## 1
-def trouble(arr, t)
-  i = 0
-  while i < arr.length-1 do
-    starting_arr_length = arr.length
-    (arr[i] + arr[i+1]) == t ? arr.delete_at(i+1) : arr
-    arr.length < starting_arr_length ? i : i += 1
-  end
-  arr
-end
+# def trouble(arr, t)
+#   i = 0
+#   while i < arr.length - 1 do
+#     starting_arr_length = arr.length
+#     (arr[i] + arr[i + 1]) == t ? arr.delete_at(i + 1) : arr
+#     arr.length < starting_arr_length ? i : i += 1
+#   end
+#   arr
+# end
 
-##2
+## 2
 def trouble(arr, t)
   loop do
     index = arr.each_cons(2).find_index { |a,b| a + b == t }
